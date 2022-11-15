7498347d64e02a040d32fc5c67319de4d78243bb
trevordcampbell <tdcampnh@gmail.com>
2022-11-15 10:36:14 +0000

    Initial Project Setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* Header */}
+      <main>
+        {/* Feature Banner */}
+        <section>
+          {/* Image / Video / Row */}
+          {/* Image / Video / Row */}
+          {/* Image / Video / Row */}
+          {/* Image / Video / Row */}
+          {/* Image / Video / Row */}
+        </section>
+      </main>
+      {/* Modal */}
+      {/* Footer */}
+    </div>
+  )
+}
+
+export default Home
